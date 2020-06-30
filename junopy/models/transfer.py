759c2e7c1355e86e11b4068457d108dcc7f19c06
franklindias <franklindias99@gmail.com>
2020-06-30 12:47:13 -0300

    Ajusta função de transferência

--- junopy/models/transfer.py
+++ junopy/models/transfer.py
@@ -4,5 +4,5 @@ from junopy.utils import get, put, post, delete
 BASE_MODEL_URL = '/transfers'
 
 
-def create(params):
-    return post(end_point=BASE_MODEL_URL, data=params)
+def create(resource_token, params):
+    return post(end_point=BASE_MODEL_URL, data=params, resource_token=resource_token)
